95f2f13a93f8c321e75628ceee96936edccde5ea
NexoByte <kheutschi@ggs.ch>
2017-07-15 19:48:39 +0200

    + plugin.yml

--- main/BungeeSystem.java
+++ main/BungeeSystem.java
@@ -1,7 +1,17 @@
 package me.nexobyte.BungeeSystem.main;
 
+import net.md_5.bungee.api.plugin.Plugin;
+
 /**
  * Created by NexoByte on 15.07.2017.
  */
-public class BungeeSystem {
+public class BungeeSystem extends Plugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
 }
